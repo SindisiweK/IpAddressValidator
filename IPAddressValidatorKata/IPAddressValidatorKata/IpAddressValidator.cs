cff231e7c63880e42e4ec0e0388f88517d4a73c1
SindisiweK <SindisiweK@sahomeloans.com>
2018-07-06 13:46:28 +0200

    ip address validator kata solution

---
+++ IPAddressValidatorKata/IPAddressValidatorKata/IpAddressValidator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+
+namespace IPAddressValidatorKata
+{
+    public class IpAddressValidator
+    {
+
+        public bool ValidateIpV4Address(string input)
+        {
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                return false;
+            }
+            if (input.EndsWith("0") || input.EndsWith("255")) return false;
+            var separator = input.Split(new char[] { '.' }, StringSplitOptions.RemoveEmptyEntries);
+
+            return separator.Length >= 4;
+        }   
+    }
+}
---
+++ IPAddressValidatorKata/IPAddressValidatorKata/IpAddressValidatorTests.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace IPAddressValidatorKata
+{
+    [TestFixture]
+    public class IpAddressValidatorTests
+    {
+        [TestCase("")]
+        [TestCase(" ")]
+        [TestCase(null)]
+        public void ValidateIpV4Address_GivenInvalidIpAddress_ShouldReturnFalse(string input)
+        {
+            //Arrange
+            var sut = new IpAddressValidator();
+            
+            //Act
+            var actual = sut.ValidateIpV4Address(input);
+
+            //Assert
+            var expected = false;
+            Assert.AreEqual(expected,actual);
+        }
+        [TestCase("1.1.1.1",true)]
+        [TestCase("192.168.1.1",true)]
+        [TestCase("10.0.0.1",true)]
+        public void ValidateIpV4Address_GivenValidIpAddress_ShouldReturnTrue(string input,bool expected)
+        {
+            //Arrange
+            var sut = new IpAddressValidator();
+
+            //Act
+            var actual = sut.ValidateIpV4Address(input);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestCase("0.0.0.0",false)]
+        [TestCase("255.255.255.255",false)]
+        [TestCase("10.0.1", false)]
+        public void ValidateIpV4Address_GivenValidIpAddressThatCannotHost_ShouldReturnFalse(string input, bool expected)
+        {
+            //Arrange
+            var sut = new IpAddressValidator();
+
+            //Act
+            var actual = sut.ValidateIpV4Address(input);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestCase("127.0.0.1", true)]
+        [TestCase("586.921.0", false)]
+        [TestCase("125.1.568.258", true)]
+        public void ValidateIpV4Address_GivenValidIpAddressThatCanHostAndCannotHost_ShouldReturnFalse(string input, bool expected)
+        {
+            //Arrange
+            var sut = new IpAddressValidator();
+
+            //Act
+            var actual = sut.ValidateIpV4Address(input);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
